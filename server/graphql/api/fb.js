fab1bb56a9e2a2d5671f490a2ecd09f7a04e2e33
Aleksandr Strikha <alex@pingbull.no>
2017-03-04 12:41:09 +0100

    add lazy fb share loading

--- server/graphql/api/fb.js
+++ server/graphql/api/fb.js
@@ -3,27 +3,30 @@ const NodeCache = require('node-cache');
 
 const shareCache = new NodeCache();
 
+let oldShareValues = {};
+
 const getShareCount = (url) => {
-    return new Promise((resolve, reject) => {
 
-        const cachedCount = shareCache.get(url);
+    const cachedCount = shareCache.get(url);
 
-        if (cachedCount !== undefined) {
-            return resolve(cachedCount);
-        }
+    if (cachedCount !== undefined) {
+        return cachedCount;
+    }
 
-        fetch(`https://graph.facebook.com/?id=${url}`)
-            .then((resp) => resp.json())
-            .then((data) => {
-                let count = 0;
-                if (!!data && !!data.share && !!data.share.share_count) {
-                    count = data.share.share_count;
-                }
-                shareCache.set(url, count, 60 * 30);
-                resolve(count);
-            });
+    fetch(`https://graph.facebook.com/?id=${url}`)
+        .then((resp) => resp.json())
+        .then((data) => {
+            let count = 0;
 
-    });
+            if (!!data && !!data.share && !!data.share.share_count) {
+                count = data.share.share_count;
+            }
+
+            shareCache.set(url, count, 60 * 30);
+            oldShareValues[url] = count;
+        });
+
+    return oldShareValues[url] || 0;
 };
 
 exports = module.exports = {
